040c5ee4e4587bdad64006c1a5129e3d38878635
CORP\karpov.roman <roman.karpov@devexpress.com>
2018-06-13 18:28:53 +0300

    Update example to simplify it.

--- CS/RegisterSummaryFunctionExample/Form1.Designer.cs
+++ CS/RegisterSummaryFunctionExample/Form1.Designer.cs
@@ -23,82 +23,66 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
             DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
             DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.StockSeriesView stockSeriesView1 = new DevExpress.XtraCharts.StockSeriesView();
+            DevExpress.XtraCharts.StockSeriesView stockSeriesView2 = new DevExpress.XtraCharts.StockSeriesView();
             DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
-            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
-            this.productsTableAdapter1 = new RegisterSummaryFunctionExample.nwindDataSetTableAdapters.ProductsTableAdapter();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.nwindDataSet1 = new RegisterSummaryFunctionExample.nwindDataSet();
-            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            this.chartControl = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(stockSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(stockSeriesView2)).BeginInit();
             this.SuspendLayout();
             // 
-            // chartControl1
+            // chartControl
             // 
-            this.chartControl1.DataAdapter = this.productsTableAdapter1;
-            this.chartControl1.DataSource = this.bindingSource1;
+            xyDiagram1.AxisX.DateTimeScaleOptions.MeasureUnit = DevExpress.XtraCharts.DateTimeMeasureUnit.Month;
             xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
             xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
-            this.chartControl1.Diagram = xyDiagram1;
-            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chartControl1.Location = new System.Drawing.Point(0, 0);
-            this.chartControl1.Name = "chartControl1";
-            this.chartControl1.PaletteName = "Office 2013";
-            series1.ArgumentDataMember = "ProductName";
-            series1.Name = "Product";
-            series1.ValueDataMembersSerializable = "UnitPrice";
-            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+            xyDiagram1.AxisY.WholeRange.AlwaysShowZeroLevel = false;
+            this.chartControl.Diagram = xyDiagram1;
+            this.chartControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right;
+            this.chartControl.Legend.Name = "Default Legend";
+            this.chartControl.Location = new System.Drawing.Point(0, 0);
+            this.chartControl.Name = "chartControl";
+            this.chartControl.PaletteName = "Office 2013";
+            series1.ArgumentDataMember = "DateTime";
+            series1.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.DateTime;
+            series1.Name = "EurUsd";
+            series1.View = stockSeriesView1;
+            this.chartControl.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
         series1};
-            this.chartControl1.Size = new System.Drawing.Size(1278, 719);
-            this.chartControl1.TabIndex = 0;
+            this.chartControl.SeriesTemplate.View = stockSeriesView2;
+            this.chartControl.Size = new System.Drawing.Size(1278, 719);
+            this.chartControl.TabIndex = 0;
             chartTitle1.Text = "Products Comparison";
-            this.chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            this.chartControl.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
             chartTitle1});
-            // 
-            // productsTableAdapter1
-            // 
-            this.productsTableAdapter1.ClearBeforeFill = true;
-            // 
-            // bindingSource1
-            // 
-            this.bindingSource1.DataMember = "Products";
-            this.bindingSource1.DataSource = this.nwindDataSet1;
-            this.bindingSource1.Sort = "";
-            // 
-            // nwindDataSet1
-            // 
-            this.nwindDataSet1.DataSetName = "nwindDataSet";
-            this.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1278, 719);
-            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.chartControl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(stockSeriesView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(stockSeriesView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private DevExpress.XtraCharts.ChartControl chartControl1;
-        private nwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
-        private System.Windows.Forms.BindingSource bindingSource1;
-        private nwindDataSet nwindDataSet1;
+        private DevExpress.XtraCharts.ChartControl chartControl;
     }
 }
 
