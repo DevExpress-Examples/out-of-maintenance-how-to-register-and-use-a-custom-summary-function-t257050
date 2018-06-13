f6761af511aea5c16092db02bf485815c6dca292
DevExpressExampleBot <fakeEmail>
2018-06-13 18:29:57 +0300

    Source auto update [skip ci]

--- VB/RegisterSummaryFunctionExample/Form1.Designer.vb
+++ VB/RegisterSummaryFunctionExample/Form1.Designer.vb
@@ -23,79 +23,63 @@
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Me.components = New System.ComponentModel.Container()
             Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
             Dim series1 As New DevExpress.XtraCharts.Series()
+            Dim stockSeriesView1 As New DevExpress.XtraCharts.StockSeriesView()
+            Dim stockSeriesView2 As New DevExpress.XtraCharts.StockSeriesView()
             Dim chartTitle1 As New DevExpress.XtraCharts.ChartTitle()
-            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            Me.productsTableAdapter1 = New RegisterSummaryFunctionExample.nwindDataSetTableAdapters.ProductsTableAdapter()
-            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-            Me.nwindDataSet1 = New RegisterSummaryFunctionExample.nwindDataSet()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.chartControl = New DevExpress.XtraCharts.ChartControl()
+            CType(Me.chartControl, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
             CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(stockSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(stockSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
-            ' chartControl1
+            ' chartControl
             ' 
-            Me.chartControl1.DataAdapter = Me.productsTableAdapter1
-            Me.chartControl1.DataSource = Me.bindingSource1
+            xyDiagram1.AxisX.DateTimeScaleOptions.MeasureUnit = DevExpress.XtraCharts.DateTimeMeasureUnit.Month
             xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
             xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-            Me.chartControl1.Diagram = xyDiagram1
-            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
-            Me.chartControl1.Name = "chartControl1"
-            Me.chartControl1.PaletteName = "Office 2013"
-            series1.ArgumentDataMember = "ProductName"
-            series1.Name = "Product"
-            series1.ValueDataMembersSerializable = "UnitPrice"
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
-            Me.chartControl1.Size = New System.Drawing.Size(1278, 719)
-            Me.chartControl1.TabIndex = 0
+            xyDiagram1.AxisY.WholeRange.AlwaysShowZeroLevel = False
+            Me.chartControl.Diagram = xyDiagram1
+            Me.chartControl.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right
+            Me.chartControl.Legend.Name = "Default Legend"
+            Me.chartControl.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl.Name = "chartControl"
+            Me.chartControl.PaletteName = "Office 2013"
+            series1.ArgumentDataMember = "DateTime"
+            series1.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.DateTime
+            series1.Name = "EurUsd"
+            series1.View = stockSeriesView1
+            Me.chartControl.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+            Me.chartControl.SeriesTemplate.View = stockSeriesView2
+            Me.chartControl.Size = New System.Drawing.Size(1278, 719)
+            Me.chartControl.TabIndex = 0
             chartTitle1.Text = "Products Comparison"
-            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
-            ' 
-            ' productsTableAdapter1
-            ' 
-            Me.productsTableAdapter1.ClearBeforeFill = True
-            ' 
-            ' bindingSource1
-            ' 
-            Me.bindingSource1.DataMember = "Products"
-            Me.bindingSource1.DataSource = Me.nwindDataSet1
-            Me.bindingSource1.Sort = ""
-            ' 
-            ' nwindDataSet1
-            ' 
-            Me.nwindDataSet1.DataSetName = "nwindDataSet"
-            Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            Me.chartControl.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
             ' 
             ' Form1
             ' 
             Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(1278, 719)
-            Me.Controls.Add(Me.chartControl1)
+            Me.Controls.Add(Me.chartControl)
             Me.Name = "Form1"
             Me.Text = "Form1"
             CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(stockSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
             CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(stockSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.chartControl, System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
 
         End Sub
 
         #End Region
 
-        Private chartControl1 As DevExpress.XtraCharts.ChartControl
-        Private productsTableAdapter1 As nwindDataSetTableAdapters.ProductsTableAdapter
-        Private bindingSource1 As System.Windows.Forms.BindingSource
-        Private nwindDataSet1 As nwindDataSet
+        Private chartControl As DevExpress.XtraCharts.ChartControl
     End Class
 End Namespace
 
